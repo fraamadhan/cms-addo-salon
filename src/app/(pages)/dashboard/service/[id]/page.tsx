3e486e575313e7b51ad2352a466a1c069488976e
fraamadhan <fakhrifajarrr@gmail.com>
2025-06-26 19:33:54 +0700

    fix: header on add service form and modify service

--- src/app/(pages)/dashboard/service/[id]/page.tsx
+++ src/app/(pages)/dashboard/service/[id]/page.tsx
@@ -3,7 +3,7 @@ import ServiceDetail from "@/app/components/dashboard/service/ServiceDetail"
 const ServiceDetailPage = () => {
     return (
         <main className="w-full p-7 flex flex-col gap-y-7">
-            <h1 className="text-3xl font-bold">Ubah Data Pengguna</h1>
+            <h1 className="text-3xl font-bold">Ubah Data Layanan</h1>
             <ServiceDetail />
         </main>
     )
